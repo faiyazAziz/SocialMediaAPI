0984789d9fc473fb9eeee9a464a77bdd54cd4526
faiyazAziz <106823454+faiyazAziz@users.noreply.github.com>
2024-01-13 14:09:22 +0530

    Update settings.py

--- soaialmedia/settings.py
+++ soaialmedia/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'soaialmedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social',
+        'USER': 'postgres',
+        'PASSWORD': '2345',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 

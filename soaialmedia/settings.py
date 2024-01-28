bcf3e38b7a58d9f18477c58c41dc74f902e93f7c
faiyazAziz <106823454+faiyazAziz@users.noreply.github.com>
2024-01-28 18:03:25 +0530

    Update settings.py

--- soaialmedia/settings.py
+++ soaialmedia/settings.py
@@ -159,8 +159,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'grdfaiyaz786@gmail.com'
-EMAIL_HOST_PASSWORD = 'moxx ftlr jmnt tudd'
+EMAIL_HOST_USER = 'faiyaz@gmail.com'
+EMAIL_HOST_PASSWORD = 'mozx fulr zmsa tuee'
 
 
 SIMPLE_JWT = {
